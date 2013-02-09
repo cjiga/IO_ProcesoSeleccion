b6344857036bb7ffb36b22e85973a54788fa0ac1
Cesar Arasaki <cesar.arasaki.88@gmail.com>
2013-02-09 02:52:25 -0500

    Nota y Prueba DAO

--- src/main/java/edu/upc/io/seleccion/dao/NotaDao.java
+++ src/main/java/edu/upc/io/seleccion/dao/NotaDao.java
@@ -1,5 +1,13 @@
 package edu.upc.io.seleccion.dao;
 
+import java.util.List;
+
+import edu.upc.io.seleccion.domain.Nota;
+
 public interface NotaDao {
 
+	
+	public int saveOrUpdate(Nota nota);
+	public List<Nota> getAll();
+	
 }
